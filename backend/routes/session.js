9785e5d28759869873fec801fa5af9c52a71f8fd
Jay Duong <jayvin.duong@gmail.com>
2024-06-17 21:12:56 -0400

    Refactoring session API backend call

--- backend/routes/session.js
+++ backend/routes/session.js
@@ -5,6 +5,7 @@ const placeholderPhotos = require('../placeholder.photos')
 const router = express.Router()
 require('dotenv').config()
 
+const AI_MAX_REQUESTS = 10 // https://platform.openai.com/docs/guides/images/usage
 const MAX_RATE_LIMIT = 1000
 const MINUTE_TIME_WINDOW = 1
 const limiter = rateLimit({
@@ -18,20 +19,39 @@ const limiter = rateLimit({
   },
 })
 
-const generatePhotos = async (params) => {
-  const { pose, sessionDurations, env } = params
-  const photos = []
+const generateDevSession = (photos, sessionDurations) => {
+  for (let i = 0; i < sessionDurations.length; i++) {
+    photos.push({
+      duration: sessionDurations[i],
+      url: placeholderPhotos[i % placeholderPhotos.length],
+    })
+  }
+}
 
-  // Use placeholder images to prevent excessive billing when testing locally.
+const createSession = async (bodyParams) => {
+  const { pose, sessionDurations, env } = bodyParams
+  let photos = []
+
+  // For local development, use local images to prevent billing.
   if (env === 'development') {
-    for (let i = 0; i < sessionDurations.length; i++) {
-      photos.push({
-        duration: sessionDurations[i],
-        url: placeholderPhotos[i % placeholderPhotos.length],
-      })
+    generateDevSession(photos, sessionDurations)
+  }
+
+  // Use OpenAI.
+  else {
+    // Number calls to OpenAI image generation, based on the max limit.
+    const apiRounds = Math.ceil(sessionDurations.length / AI_MAX_REQUESTS)
+    for (let i = 0; i < apiRounds; i++) {
+      generateSessionPhotos(photos, sessionDurations.splice(0, AI_MAX_REQUESTS))
     }
   }
 
+  return new Promise((resolve, reject) => {
+    resolve({ env, pose, photos })
+  })
+}
+
+const generateSessionPhotos = async (photos, sessionDurations) => {
   // OpenAI image API request body params.
   // https://platform.openai.com/docs/guides/images/usage
   // {
@@ -39,7 +59,6 @@ const generatePhotos = async (params) => {
   //   "n": 2,
   //   "size": "1024x1024"
   // }
-
   // OpenAI image response example.
   // {
   //   "created": ...,
@@ -48,30 +67,16 @@ const generatePhotos = async (params) => {
   //       ...
   //   ]
   // }
-
-  return new Promise((resolve, reject) => {
-    resolve({
-      env,
-      pose,
-      photos,
-    })
-  })
 }
 
-router
-  .get('/', async (req, res) => {
-    res.send({
-      message: 'Session',
-    })
-  })
-  .post('/create', limiter, async (req, res) => {
-    // Generate photos.
-    const message = await generatePhotos(req.body)
+router.post('/create', limiter, async (req, res) => {
+  // Generate photos.
+  const message = await createSession(req.body)
 
-    await res.send({
-      success: true,
-      data: message,
-    })
+  await res.send({
+    success: true,
+    data: message,
   })
+})
 
 module.exports = router
